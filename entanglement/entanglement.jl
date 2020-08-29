ad9ffdeeeb2f2837261acc3b85ae42af5178894a
Sizumi <souta120789@gmail.com>
2020-08-30 03:06:56 +0900

    entanglement: fail Rxx & split code

--- entanglement/entanglement.jl
+++ entanglement/entanglement.jl
@@ -55,54 +55,11 @@ hist = fit(Histogram, Int.(results), 0:2^4)
 
 GHZ4_graff = bar(hist.edges[1] .- 0.5, hist.weights, legend =:none)
 
-
-
-# https://qiita.com/YuichiroMinato/items/5e85a96e9279247425f2
-
-n = 2
-modern_Bell = chain(
-    n,
-    put(2=>H),
-    control(2, 1=>Rx(pi/2))
-    # どうもこれはRXXじゃないらしい
-)
-
-apply!(zero_state(2), modern_Bell)
-
-results = zero_state(2) |> modern_Bell |> r->measure(r, nshots = 1000)
-
-hist = fit(Histogram, Int.(results), 0:2^n)
-
-MBell_graff = bar(hist.edges[1] .- 0.5, hist.weights, legend =:none)
-
 # GHZのグラフを一つにまとめて保存
 
-A = plot(Bell_graff, GHZ3_graff, GHZ4_graff, MBell_graff, legend =:none)
+A = plot(Bell_graff, GHZ3_graff, GHZ4_graff, legend =:none)
 
 path_png = "program/julia_code/plot_practice/entanglement.png"
 path_svg = "program/julia_code/plot_practice/entanglement.svg"
 
 savefig(A, path_svg)
-
-
-# 角度の変化をアニメーションにできないか？
-# 結論：yao.jlでRXXゲートが見つからない...
-
-# アニメーションのインスタンス生成
-anim = Animation()
-
-for x = 0:0.05π:2π
-    m_Bell = chain(
-        2,
-        put(2=>H),
-        control(2, 1=>Rx(x))
-    )
-
-    apply!(zero_state(2), m_Bell)
-    results = zero_state(2) |> m_Bell |> r->measure(r, nshots = 1000)
-    hist = fit(Histogram, Int.(results), 0:4)
-    MBell_plt = bar(hist.edges[1] .- 0.5, hist.weights, label = "θ = $x", ylims = (0, 1000))
-    frame(anim, MBell_plt)
-end
-
-gif(anim,"anim_entangle.gif", fps = 10)
